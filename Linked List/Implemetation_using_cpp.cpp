ae9a3c7bd65aaedd7cc2c4e52075826b93ea9b1b
iashu-777 <2k23.csds2310610@gmail.com>
2024-07-13 12:03:41 +0530

    hhh

--- Linked List/Implemetation_using_cpp.cpp
+++ Linked List/Implemetation_using_cpp.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
 using namespace std;
+struct Node{
+    int data;
+    Node *next;
+    Node(int x)
+    {
+        data=x;
+        next=NULL;
+    } 
+};
 int main(){
-    cout<<"hello world ";
+    Node *head=new Node(10);
+    Node *temp1=new Node(20);
+    Node *temp2=new Node(30);
+    head->next=temp1;
+    temp1->next=temp2;
+    Node *ptr=head;
+    while(ptr!=NULL){
+        cout<<ptr->data<<"->";
+        ptr=ptr->next;
+    }
+    cout<<"NULL";
     return 0;
 }
