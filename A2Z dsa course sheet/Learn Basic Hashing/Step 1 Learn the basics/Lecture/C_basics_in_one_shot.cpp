7f9ae1452d545e510db89ce2ec12b7ec690a1635
iashu-777 <2k23.csds2310610@gmail.com>
2024-08-04 22:05:23 +0530

    new

---
+++ A2Z dsa course sheet/Learn Basic Hashing/Step 1 Learn the basics/Lecture/C_basics_in_one_shot.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s1="hello";
+    string s2="bhai";
+    string s=s1+s2;
+    cout<<"hello i am learning cpp"<<"\n";
+    cout<<s;
+}
