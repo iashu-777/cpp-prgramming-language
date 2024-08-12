4fbd55f81518b27f8c059437aaec89dfd0640e9f
iashu-777 <2k23.csds2310610@gmail.com>
2024-08-12 21:02:26 +0530

    hey

--- A2Z dsa course sheet/Learn Basic Hashing/Step 1 Learn the basics/Lecture/C_basics_in_one_shot.cpp
+++ A2Z dsa course sheet/Learn Basic Hashing/Step 1 Learn the basics/Lecture/C_basics_in_one_shot.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-    string s1="hello";
-    string s2="bhai";
-    string s=s1+s2;
-    cout<<"hello i am learning cpp"<<"\n";
-    cout<<s;
+//     string s1="hello";
+//     string s2="bhai";
+//     string s=s1+s2;
+//     cout<<"hello i am learning cpp"<<"\n";
+// cout<<s;
+
+//  string str;
+//  cin>>str;
+//  cout<<str;
+ //for reading a full line we use get line function
+
+ string str;
+ getline(cin,str);
+ cout<<str;
+
 }
