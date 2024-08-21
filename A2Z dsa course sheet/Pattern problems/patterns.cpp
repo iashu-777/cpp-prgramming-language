cc5720f0ca593b9917d9e485516cfa5daa93dbd3
iashu-777 <2k23.csds2310610@gmail.com>
2024-08-21 14:27:14 +0530

    hey

--- A2Z dsa course sheet/Pattern problems/patterns.cpp
+++ A2Z dsa course sheet/Pattern problems/patterns.cpp
@@ -8,6 +8,11 @@ void print1(int n){
         }
         cout<<endl;
     }
+// * * * * * 
+// * * * * * 
+// * * * * * 
+// * * * * * 
+// * * * * * 
 }
 void print2(int n){
     for(int i=0;i<n;i++){
@@ -16,10 +21,88 @@ void print2(int n){
         }
         cout<<endl;
     }
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * * 
+
+}
+void print3(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+// 1
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5 
+
+}
+void print4(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+// 1
+// 2 2 
+// 3 3 3 
+// 4 4 4 4 
+// 5 5 5 5 5 
+
+}
+void print5(int n){
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
+
+}
+void print6(int n){
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+// 1 2 3 4 5 
+// 1 2 3 4 
+// 1 2 3 
+// 1 2 
+// 1 
+}
+void print7(int n){
+    for(int i=1;i<=n;i++){
+        for(int k=1;k<=n-i;k++){
+            cout<<" ";
+        }
+        for(int j=1;j<=2*i-1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
 }
 int main(){
     int n;
     cin>>n;
-    print2(n);
+    print7(n);
 
 }
