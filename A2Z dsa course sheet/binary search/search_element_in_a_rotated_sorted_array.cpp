3edf65a5b09a6d2ccf97fd17746c60729dd894a3
iashu-777 <2k23.csds2310610@gmail.com>
2024-09-02 05:11:48 +0530

    hey

---
+++ A2Z dsa course sheet/binary search/search_element_in_a_rotated_sorted_array.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+int search(vector<int>&arr,int n,int k){
+    int low=0,high=n-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]==k) return mid;
+        
+        if(arr[low]<=arr[mid]){ //is left sorted
+            if(arr[low]<=k && k<=arr[mid]){//is present in left half
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        else{
+            if(arr[mid]<=k && k<=arr[high]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+    }
+    return -1;
+}
+int main(){
+    int n;
+    cin>>n;
+    int t;
+    vector<int>arr;
+    for(int i=0;i<n;i++){
+        cin>>t;
+        arr.push_back(t);
+    }
+    int key;
+    cin>>key;
+    cout<<search(arr,n,key);
+}
