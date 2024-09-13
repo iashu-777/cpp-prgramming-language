be9f3e84d593bbe67d880332becd3544502ba014
iashu-777 <2k23.csds2310610@gmail.com>
2024-09-13 07:18:34 +0530

    hey

---
+++ A2Z dsa course sheet/binary search/peakelement.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+
+        if (nums.size() == 1) {
+            return 0;
+        }
+        if (nums[0] > nums[1])
+            return 0;
+        if (nums[n - 1] > nums[n - 2])
+            return n - 1;
+
+        int low = 1;
+        int high = nums.size() - 2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1]) {
+                return mid;
+            } else if (nums[mid] > nums[mid-1]) {
+                low = mid + 1;
+            } else  {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
