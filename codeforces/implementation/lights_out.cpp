1d46832f1f77c47f4329699ef2efb7d2ea1a7ccd
Ashish Kumar Gupta <2k23.csds2310610@gmail.com>
2024-12-26 13:29:23 +0530

    hey

---
+++ codeforces/implementation/lights_out.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t=3;
+    vector<vector<int>>toggle;
+    vector<vector<int>>ans;
+    for(int i=0;i<3;i++){
+        ans.push_back({1,1,1});
+    }
+    while(t--){
+        int i,j,k;
+        cin>>i>>j>>k;
+        toggle.push_back({i,j,k});
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(toggle[i][j]!=0){
+                if(toggle[i][j]%2!=0){
+                    if(i>0)
+                    ans[i-1][j]=!ans[i-1][j];
+                    if(j>0)
+                    ans[i][j-1]=!ans[i][j-1];
+                    if(i<2)
+                    ans[i+1][j]=!ans[i+1][j];
+                    if(j<2)
+                    ans[i][j+1]=!ans[i][j+1];
+                    ans[i][j]=!ans[i][j];
+                    
+                }
+            }
+        }
+    }
+    for(auto x:ans){
+        for(auto t:x){
+            cout<<t;
+        }
+        cout<<endl;
+    }
+
+}
